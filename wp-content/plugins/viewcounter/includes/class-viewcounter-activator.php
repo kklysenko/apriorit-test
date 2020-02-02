90671b0d843e422a380c956147843a146e280f71
IamBrave <gameron27@gmail.com>
2020-02-02 17:04:15 +0200

    added posts and plugin

---
+++ wp-content/plugins/viewcounter/includes/class-viewcounter-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/IamBrave
+ * @since      1.0.0
+ *
+ * @package    Viewcounter
+ * @subpackage Viewcounter/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Viewcounter
+ * @subpackage Viewcounter/includes
+ * @author     K Lysenko <gameron27@gmail.com>
+ */
+class Viewcounter_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ wp-content/plugins/viewcounter/includes/class-viewcounter-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/IamBrave
+ * @since      1.0.0
+ *
+ * @package    Viewcounter
+ * @subpackage Viewcounter/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Viewcounter
+ * @subpackage Viewcounter/includes
+ * @author     K Lysenko <gameron27@gmail.com>
+ */
+class Viewcounter_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
